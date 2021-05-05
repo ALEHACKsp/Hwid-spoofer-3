d79e91ed0ae607ee599eba4d46b2a642d5e50fd6
hotcolt <66889225+hotcolt@users.noreply.github.com>
2021-05-05 01:09:00 -0300

    Add files via upload

---
+++ HWID SPOOFER/HWID Spoofer/Misc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWID_Spoofer
+{
+    class Misc
+    {
+        private static int number;
+        public static void DrawASCII()
+        {
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                               ██░ ██ ▓█████  ███▄    █ ▄▄▄█████▓ ▄▄▄       ██▓");
+            Console.WriteLine("                              ▓██░ ██▒▓█   ▀  ██ ▀█   █ ▓  ██▒ ▓▒▒████▄    ▓██▒");
+            Console.WriteLine("                              ▒██▀▀██░▒███   ▓██  ▀█ ██▒▒ ▓██░ ▒░▒██  ▀█▄  ▒██▒");
+            Console.WriteLine("                              ░▓█ ░██ ▒▓█  ▄ ▓██▒  ▐▌██▒░ ▓██▓ ░ ░██▄▄▄▄██ ░██░");
+            Console.WriteLine("                              ░▓█▒░██▓░▒████▒▒██░   ▓██░  ▒██▒ ░  ▓█   ▓██▒░██░");
+            Console.WriteLine("                               ▒ ░░▒░▒░░ ▒░ ░░ ▒░   ▒ ▒   ▒ ░░    ▒▒   ▓▒█░░▓  ");
+            Console.WriteLine("                               ▒ ░▒░ ░ ░ ░  ░░ ░░   ░ ▒░    ░      ▒   ▒▒ ░ ▒ ░");
+            Console.WriteLine("                               ░  ░░ ░   ░      ░   ░ ░   ░        ░   ▒    ▒ ░");
+            Console.WriteLine("                               ░  ░  ░   ░  ░         ░                ░  ░ ░  ");
+            Console.WriteLine("                                                                               ");
+
+     
+   
+        }
+        public static void HashText(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\n[");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("#");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("] ");
+            Console.Write(text);
+        }
+
+        public static void OptionText(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\n[");
+            Console.ForegroundColor = ConsoleColor.White;
+            number++;
+            Console.Write(number);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("] ");
+            Console.Write(text);
+        }
+
+        private static void ResetOption()
+        {
+
+            number = 0;
+
+        }
+    }
+
+
+}
---
+++ HWID SPOOFER/HWID Spoofer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWID_Spoofer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hentai Spoofer | V1.0 | hanime.tv";
+            Misc.DrawASCII();
+            Misc.HashText("Welcome To Your First Spoofer");
+            Misc.HashText("Press Any Key To Load Options...");
+            Console.ReadKey();
+            Console.Clear();
+            Misc.DrawASCII();
+            Misc.OptionText("spoof");
+            Misc.OptionText("clean");
+            Misc.HashText("Option :");
+            var option = Console.ReadLine();
+            if (option == "1")
+            {
+                Console.Clear();
+                Misc.DrawASCII();
+                Misc.HashText("Spoofing Diskdrive!");
+                Spoof.SpoofHDD();
+                Misc.HashText("Spoofed Diskdrive!");
+                Console.ReadKey();
+
+            }
+            else if (option == "2")
+            {
+                Console.Clear();
+                Misc.DrawASCII();
+                Misc.HashText("Cleaning Traces!");
+                Spoof.CleanTraces(@"C:\Users\Raymond Shell\Pictures\mapper.exe");
+                Misc.HashText("Cleaned Traces!");
+                Console.ReadKey();
+
+            }
+            else
+            {
+                Misc.HashText("OK retard Maybe Type Right Next Time");
+            }
+        }
+    }
+}
